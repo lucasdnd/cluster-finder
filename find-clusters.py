f43694f3366c0f06674e554f76a145cf3f2adcd1
Tulio <tulio@doubleleft.com>
2015-07-29 20:32:50 -0300

    Improved code

--- find-clusters.py
+++ find-clusters.py
@@ -16,10 +16,9 @@ image = Image.open(file_name)
 bin_image = image.convert("1")
 width, height = bin_image.size
 
-clusters = [] # A list of lists of tuples
-current_cluster = [] # Current cluster being investigated
-checked = [] # A list of tuples (pixels) that have already been checked
-inside_cluster = False
+clusters = [] # A list of lists of pixels (tuples)
+checked = [] # A list of pixels (tuples) that have already been checked
+current_cluster = []
 
 def find_next(x, y, direction, not_found_count):
 
@@ -44,48 +43,48 @@ def find_next(x, y, direction, not_found_count):
 
   # Go!
   pixel = bin_image.getpixel((x, y))
-  inside_cluster = (pixel == 255) and ((x, y) not in checked)
-  if inside_cluster:
-    not_found_count = 0
-    current_cluster.append((x, y))
-    checked.append((x, y))
-    find_next(x, y, direction, not_found_count)
-  else:
-    # if (x, y) not in checked:
+
+  if pixel == 0:
     checked.append((x, y))
     direction = change_direction(direction)
-    not_found_count = not_found_count + 1
+
+    # Stop condition
+    not_found_count += 1
     if not_found_count > 3:
       return
+    
     find_next(x, y, direction, not_found_count)
 
+  elif (x, y) not in checked:
+    current_cluster.append((x, y))
+    checked.append((x, y))
+    find_next(x, y, direction, 0)
+
 def change_direction(direction):
-  direction = direction + 1
+  direction += 1
   if direction > 3:
     direction = 0
   return direction
 
 # Search for clusters!
-for y in range(0, height):
-  for x in range(0, width):
+for j in range(0, height):
+  for i in range(0, width):
 
-    pixel = bin_image.getpixel((x, y))
-    inside_cluster = (pixel == 255) and ((x, y) not in checked)
+    current_cluster = []
+    pixel = bin_image.getpixel((i, j))
     
-    if inside_cluster:
-      current_cluster.append((x, y))
-      checked.append((x, y))
-      find_next(x, y, 0, 0)
-      print("found cluster with " + str(len(current_cluster)) + " pixels")
-      clusters.append(current_cluster)
-      current_cluster = []
-    else:
-      # if (x, y) not in checked:
-      checked.append((x, y))
+    if pixel == 0:
+      checked.append((i, j))
+      continue
 
-print("number of clusters found: " + str(len(clusters)))
-print("")
+    elif (i, j) not in checked:
+      current_cluster.append((i, j))
+      checked.append((i, j))
+      find_next(i, j, 0, 0)
+
+      if len(current_cluster) > 1:
+        print("found cluster with " + str(len(current_cluster)) + " pixels")
+        clusters.append(current_cluster)
+
+print("clusters found: " + str(len(clusters)))
 print("pixels checked: " + str(len(checked)))
-
-
-
